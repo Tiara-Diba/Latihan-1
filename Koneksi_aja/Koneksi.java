8565bacaf7ed6e02c8794a7cb49f0942c243a431
Tiara-Diba <80020178+Tiara-Diba@users.noreply.github.com>
2021-06-05 00:22:56 +0700

    Add files via upload

---
+++ Koneksi_aja/Koneksi.java
@@ -0,0 +1,28 @@
+//Nama      : Tiara Diba
+//NIM       : 11200930000017
+//Kelas     : 2A SI'20
+
+package Koneksi_aja;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Koneksi {
+     private Connection koneksi;
+    
+    public Connection connect(){
+     try{
+    Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Berhasil Koneksi");
+    } catch (ClassNotFoundException ex){
+            System.out.println("Gagal Koneksi" + ex);
+    }
+    String url = "jdbc:mysql://localhost:3306/db_luki_silk";
+    try{
+    koneksi = DriverManager.getConnection(url, "root", "");
+            System.out.println("Berhasil Koneksi Database");
+    } catch (SQLException ex){
+            System.out.println("Gagal Koneksi Database" + ex);
+    }
+    return koneksi;
+    }
+}
